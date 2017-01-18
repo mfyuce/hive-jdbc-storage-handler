3e743d60f9216f1ced1f8af3f0ebece77eac7423
myuce <mehmet.yuce@organonanalytics.com>
2017-01-18 17:53:23 +0300

    to work with Hortonworks Data Platform 2.5

--- src/main/java/com/qubitproducts/hive/storage/jdbc/JdbcStorageHandler.java
+++ src/main/java/com/qubitproducts/hive/storage/jdbc/JdbcStorageHandler.java
@@ -15,22 +15,31 @@
  */
 package com.qubitproducts.hive.storage.jdbc;
 
+import com.qubitproducts.hive.storage.jdbc.conf.JdbcStorageConfigManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.HiveMetaHook;
+import org.apache.hadoop.hive.ql.index.IndexPredicateAnalyzer;
+import org.apache.hadoop.hive.ql.index.IndexSearchCondition;
+import org.apache.hadoop.hive.ql.metadata.DefaultStorageHandler;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.metadata.HiveStorageHandler;
+import org.apache.hadoop.hive.ql.metadata.HiveStoragePredicateHandler;
+import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
+import org.apache.hadoop.hive.ql.plan.ExprNodeGenericFuncDesc;
 import org.apache.hadoop.hive.ql.plan.TableDesc;
 import org.apache.hadoop.hive.ql.security.authorization.HiveAuthorizationProvider;
+import org.apache.hadoop.hive.serde2.Deserializer;
 import org.apache.hadoop.hive.serde2.SerDe;
 import org.apache.hadoop.mapred.InputFormat;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.OutputFormat;
 
-import com.qubitproducts.hive.storage.jdbc.conf.JdbcStorageConfigManager;
+import java.util.*;
 
-import java.util.Map;
-import java.util.Properties;
-
-public class JdbcStorageHandler implements HiveStorageHandler {
+public class JdbcStorageHandler extends DefaultStorageHandler implements
+        HiveStoragePredicateHandler {//implements HiveStorageHandler {
+    private static final Log LOG = LogFactory.getLog(JdbcStorageHandler.class);
 
     private Configuration conf;
 
@@ -96,6 +105,62 @@ public class JdbcStorageHandler implements HiveStorageHandler {
     @Override
     public HiveAuthorizationProvider getAuthorizationProvider() throws HiveException {
         return null;
+    }
+
+/*
+* from https://github.com/qubole/Hive-JDBC-Storage-Handler
+* org/apache/hadoop/hive/jdbc/storagehandler/JdbcStorageHandler.java
+* */
+    @Override
+    public DecomposedPredicate decomposePredicate(JobConf jobConf,
+                                                  Deserializer deserializer, ExprNodeDesc predicate) {
+
+        IndexPredicateAnalyzer analyzer = new IndexPredicateAnalyzer();
+        // adding Comaprison Operators
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPEqual");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPEqualOrGreaterThan");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPEqualOrLessThan");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPLessThan");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPGreaterThan");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFIn");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFBetween");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPNot");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPNotNull");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPNull");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPNotEqual");
+        analyzer.addComparisonOp("org.apache.hadoop.hive.ql.udf.generic.GenericUDFOPAnd");
+
+        // getting all column names
+        String columnNames = jobConf
+                .get(org.apache.hadoop.hive.serde.serdeConstants.LIST_COLUMNS);
+
+        StringTokenizer st = new StringTokenizer(columnNames, ",");
+        // adding allowed column names
+        while (st.hasMoreTokens()) {
+            String columnName = (String) st.nextToken();
+            analyzer.allowColumnName(columnName);
+            LOG.info(columnName);
+        }
+        // filtering out residual and pushed predicate
+        List<IndexSearchCondition> searchConditions = new ArrayList<IndexSearchCondition>();
+        ExprNodeDesc residualPredicate = analyzer.analyzePredicate(predicate,
+                searchConditions);
+
+        for (IndexSearchCondition e : searchConditions) {
+            LOG.info("COnditions fetched are: " + e.toString());
+        }
+
+        DecomposedPredicate decomposedPredicate = new DecomposedPredicate();
+        decomposedPredicate.pushedPredicate = (ExprNodeGenericFuncDesc) analyzer
+                .translateSearchConditions(searchConditions);
+        decomposedPredicate.residualPredicate = (ExprNodeGenericFuncDesc) residualPredicate;
+        if (decomposedPredicate.pushedPredicate != null)
+            LOG.info("Predicates pushed: "
+                    + decomposedPredicate.pushedPredicate.getExprString());
+        if (decomposedPredicate.residualPredicate != null)
+            LOG.info("Predicates not Pushed: "
+                    + decomposedPredicate.residualPredicate.getExprString());
+        return decomposedPredicate;
     }
 
 }
